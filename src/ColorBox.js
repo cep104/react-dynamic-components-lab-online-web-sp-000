d144a19fd7333d7c8d73da238fb7716bbdf65cdc
cep104 <candicelizabethpeters@gmail.com>
2021-01-04 20:33:10 -0600

    notes

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,20 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class ColorBox extends Component {
-
   state = {
-    todos: [
-      
-    ]
-  }
+    todos: [],
+  };
 
   render() {
-    return (
-      <div className="color-box" style={{opacity: null /*replace null with the value*/}}>
-        {/* your conditional code here! */}
-      </div>
-    )
+    //basecase
+    if (this.props.opacity >= 0.2) {
+      //as long as opacity is greather than or equal to .2
+      return (
+        <div className="color-box" style={{ opacity: this.props.opacity }}>
+          <ColorBox opacity={this.props.opacity - 0.1} />
+        </div>
+      );
+    } else {
+      //as soon as it doesn't meet the condition exit (return null)
+      return null;
+    }
   }
-
 }
 
+//// Alternative solution:
+
+// export default class ColorBox extends Component {
+
+//   render() {
+//     return (
+//       <div className="color-box" style={{opacity: this.props.opacity}}>
+//         {this.props.opacity >= 0.2 ? <ColorBox opacity={this.props.opacity - 0.1} /> : null}
+//       </div>
+//     )
+//   }
+
+// }
